24beff3bafff68103b644bf44aea8b27f88cb1c0
JJieunn <lancia1228@gamil.com>
2022-11-27 23:50:30 +0900

    Rename: userController 파일명 수정
    userController 파일명 수정
      - 파일명 수정
    
    Issue: 없음

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import userController from "../controllers/usersController"
+import userController from "../controllers/userController"
 
 const router = express.Router()
 
@@ -7,7 +7,7 @@ router.post('/duplication', userController.userAvailableCheck)
 router.post('/signup', userController.createUser)
 router.post('/signin', userController.signInUser)
 router.post('/kakaoLogin', userController.kakaoLogin)
-router.post('/kakaoLogout', userController.kakaoLogout) // 굳이 카카오와 일반 로그아웃을 구별?
+router.post('/kakaoLogout', userController.kakaoLogout)
 
 
 export default router
