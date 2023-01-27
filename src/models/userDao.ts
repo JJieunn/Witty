c6869a21d676e7a3de05c38b367949c800486cac
JJieunn <lancia1228@gamil.com>
2023-01-27 23:12:32 +0900

    Update: 마이페이지 내 게시글 목록 조회 기능 수정
    마이페이지 내 게시글 목록 조회 기능 수정
      - 기존의 내 게시글 목록 조회는 북마크 여부가 표시되지 않음
      - 내 게시글이어도 북마크를 할 수 있으므로 is_marked도 같이 표시되도록 SQL문 수정
    
    Issue: #51

--- src/models/userDao.ts
+++ src/models/userDao.ts
@@ -92,39 +92,32 @@ const updateUserName = async (updateData: UpdateUserDTO) => {
 
 
 const getMyPosts = async (userId: number): Promise<returnPostDTO[]> => {
+  let likeAndBookmark = ""
+  let leftJoinWithLikes = ""
+  let leftJoinWithBookmarks = ""
+
+  if(userId != null) {
+    likeAndBookmark = `, likes.is_liked as is_liked, bookmarks.is_marked as is_marked`
+    leftJoinWithLikes = `LEFT JOIN (SELECT post_id, is_liked FROM post_likes WHERE user_id = ${userId}) likes ON p.id = likes.post_id`
+    leftJoinWithBookmarks = `LEFT JOIN (SELECT post_id, is_marked FROM post_bookmarks WHERE user_id = ${userId}) bookmarks ON p.id = bookmarks.post_id`
+  }
+
   return await myDataSource.query(`
   SELECT 
       p.id, u.nickname, p.user_id, p.category_id as category,
-      p.content, p.created_at, c.count_comments, pl.count_likes,
-      pi.images
+      p.content, p.created_at, c.count_comments, pl.count_likes
+      ${likeAndBookmark}, pi.images
     FROM posts p
     JOIN users u ON p.user_id = u.id
+    ${leftJoinWithLikes}
+    ${leftJoinWithBookmarks}
     LEFT JOIN (SELECT post_id, JSON_ARRAYAGG(image_url) as images FROM post_images GROUP BY post_id) pi ON p.id = pi.post_id
     LEFT JOIN (SELECT post_id, COUNT(id) as count_comments FROM comments GROUP BY post_id) c ON p.id = c.post_id
     LEFT JOIN (SELECT post_id, COUNT(id) as count_likes FROM post_likes WHERE is_liked = 1 GROUP BY post_id) pl ON p.id = pl.post_id 
     WHERE p.user_id = ?
     ORDER BY p.created_at DESC
   `, [userId])
-  /* return await myDataSource.getRepository(Posts)
-    .createQueryBuilder("posts")
-    .select(["posts.id AS id", "posts.content AS content", "posts.created_at AS created_at",
-        "post_images.image_url AS images",
-        "posts.category_id AS category"])
-    .addSelect("COUNT(comments.id) AS count_comments")
-    .addSelect("COUNT(post_likes.id) AS count_likes")
-    .leftJoin("posts.comments", "comments")
-    .leftJoin("posts.post_likes", "post_likes")
-    .leftJoin("posts.post_images", "post_images")
-    .where("posts.user_id = :userId", { userId })
-    .groupBy("posts.id")
-    .orderBy("posts.created_at", "DESC")
-    .getRawMany()
-    */
-} 
-/* 
-  LEFT JOIN 
-    ( SELECT post_id, JSON_ARRAYAGG(image_url) as images FROM post_images GROUP BY post_id ) pi ON p.id = pi.post_id
-*/
+}
 
 
 const getMyBookmarks = async (userId: number) => {
